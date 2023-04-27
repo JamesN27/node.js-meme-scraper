ea9058bdf5e57bb03cd9a82a9075f5e2cf834071
James Henning <jamesniklas@hotmail.de>
2023-04-27 17:42:27 +0200

    Delete one single comment höhö

--- index.js
+++ index.js
@@ -10,42 +10,22 @@ if (!fs.existsSync(folderName)) {
 }
 
 // get HTML from target website
-const response = await fetch('https://memegen-link-examples-upleveled.netlify.app/');
+const response = await fetch(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+);
 const body = await response.text();
 
 // extract URLs from response HTML
 const urls = extractUrls(body);
 
 // filter URLs so we only get the first 10 that are definitely images (containing the keyword width)
-const filteredUrls = urls.filter(url => url.includes('width')).slice(0,10);
+const filteredUrls = urls.filter((url) => url.includes('width')).slice(0, 10);
 
 // loop to safe the images to the target folder and naming them correctly
 for (let i = 1; i <= filteredUrls.length; i++) {
-    const response2 = await fetch(urls[i]);
-    const buffer = await response2.buffer();
-    const fileName = `${folderName}/0${i}.jpg`;
-    fs.writeFileSync(fileName, buffer);
-  }
-  
-
-
-
-
-
-
-
-
-
-
-// console.log(filteredUrls);
-
-
-
-
-
-
-
-
-
-
+  const response2 = await fetch(urls[i]);
+  const buffer = await response2.buffer();
+  const fileName = `${folderName}/0${i}.jpg`;
+  fs.writeFileSync(fileName, buffer);
+}
 
